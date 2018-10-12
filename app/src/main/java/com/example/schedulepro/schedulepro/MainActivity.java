cbf4ebea0ae16b852933fc853996ece1cc70e04c
chenkksho <530910789@qq.com>
2018-10-12 15:12:32 +0800

    增加了一个button和一个text

--- app/src/main/java/com/example/schedulepro/schedulepro/MainActivity.java
+++ app/src/main/java/com/example/schedulepro/schedulepro/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.apache.http.client.HttpClient;
@@ -16,6 +18,7 @@ import org.apache.http.util.EntityUtils;
 
 import android.os.Handler;
 import android.os.Message;
+import android.widget.Toast;
 
 import java.util.function.Function;
 
@@ -23,12 +26,14 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
     private int index = 0;
+    private String userName = "";
     private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            mTextMessage.setText("加载数据ing...");
+
+            mTextMessage.setText(String.format("加载%s数据ing...",item.getTitle()));
             thead.start();
             //
             return true;
@@ -42,8 +47,11 @@ public class MainActivity extends AppCompatActivity {
     private Thread thead = new Thread(new Runnable() {
         @Override
         public void run() {
+            if(userName==""){
+                return;
+            }
             // 使用GET方法发送请求,需要把参数加在URL后面，用？连接，参数之间用&分隔
-            String url = "http://www.superps.cn:1337/com/hello?孟光";
+            String url = "http://www.superps.cn:1337/com/hello?" + userName;
 
             // 生成请求对象
             HttpGet httpGet = new HttpGet(url);
@@ -51,20 +59,14 @@ public class MainActivity extends AppCompatActivity {
 
             // 发送请求
             try {
-
                 HttpResponse response = httpClient.execute(httpGet);
                 final String result = EntityUtils.toString(response.getEntity(), "utf-8");
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        mTextMessage.setText(result + "--" + (index++));
+                        mTextMessage.setText(result);
                     }
                 });
-                //Message msg = new Message();
-                //msg.obj = result + "--" + (index++);
-                //thead.sleep(1000);
-                //handler.handleMessage(msg);
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -77,13 +79,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        //MainActivity.this.runOnUiThread(thead);
         thead.start();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-//        int id=navigation.getMenu().
-//        MenuItem item= (MenuItem)findViewById(id);
-//        mTextMessage.setText(id+"");
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Button btnCommit = (Button) findViewById(R.id.commit);
+        btnCommit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(MainActivity.this,"asd",Toast.LENGTH_LONG).show();
+                TextView txtUsr = (TextView) findViewById(R.id.txt);
+                MainActivity.this.userName = txtUsr.getText().toString();
+                thead.start();
+            }
+        });
     }
 
 }
